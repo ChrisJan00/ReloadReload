f5325572c316140483fa3fa8a58e6f6271883381
ChrisJan00 <cjan_ynzn@yahoo.es>
2014-05-25 11:30:31 +0200

    juicing up explosions

--- game/collisions.lua
+++ game/collisions.lua
@@ -12,18 +12,26 @@ function collisions.spawnExplosion(_x, _y, _zz)
     x = _x,
     y = _y,
     zz = _zz,
-    prescale = 2 / ( (_zz - monsters.z0) * monsters.zscale + monsters.zbias ),
+    prescale = 10 / ( (_zz - monsters.z0) * monsters.zscale + monsters.zbias ),
+    amp = 0,
+    opacity = 1,
  
     update = function (self, dt)
       self.timeLeft = self.timeLeft - dt
       if self.timeLeft <= dt then
         self:kill()
       end
+
+      local t = (defaultTime - self.timeLeft)/defaultTime * 5
+      self.amp = t * math.exp(-t) * self.prescale
+      if t>1 then
+        self.opacity = self.opacity * math.pow(0.9, dt*60)
+      end
     end,
 
     draw = function (self)
-      love.graphics.setColor(255,255,255)
-      love.graphics.draw(explosionPic, self.x, self.y,0,screenScale*self.prescale,screenScale*self.prescale,explosionPic:getWidth()*0.5,explosionPic:getHeight()*0.5)
+      love.graphics.setColor(255,255,255,255*self.opacity)
+      love.graphics.draw(explosionPic, self.x, self.y,0,self.amp * screenScale,self.amp * screenScale,explosionPic:getWidth()*0.5,explosionPic:getHeight()*0.5)
     end,
 
     kill = monsters.killFunc
